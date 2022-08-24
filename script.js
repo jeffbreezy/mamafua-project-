5956a0472eb9e14be33ab4c2dc4f9a132efc7e96
Geoffrey Mwangi <73704415+jeffbreezy@users.noreply.github.com>
2022-08-24 10:42:46 +0300

    Add files via upload
    the website is yet to be completed.

---
+++ script.js
@@ -0,0 +1,78 @@
+$(window).on("hashchange", function(){
+	if(location.hash.slice(1)=="register"){
+		$(".card").addClass("extend");
+		$("#login").removeClass("selected");
+		$("#register").addClass("selected");
+	} else {
+		$(".card").removeClass("extend");
+		$("#login").addClass("selected");
+		$("#register").removeClass("selected");
+	}
+});
+$(window).trigger("hashchange");
+let menu = document.querySelector('.menu');
+let toggle = document.querySelector('.toggle');
+
+toggle.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
+const forms = document.querySelector(".forms"),
+      pwShowHide = document.querySelectorAll(".eye-icon"),
+      links = document.querySelectorAll(".link");
+
+pwShowHide.forEach(eyeIcon => {
+    eyeIcon.addEventListener("click", () => {
+        let pwFields = eyeIcon.parentElement.parentElement.querySelectorAll(".password");
+        
+        pwFields.forEach(password => {
+            if(password.type === "password"){
+                password.type = "text";
+                eyeIcon.classList.replace("bx-hide", "bx-show");
+                return;
+            }
+            password.type = "password";
+            eyeIcon.classList.replace("bx-show", "bx-hide");
+        })
+        
+    })
+})      
+
+/* services*/
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
+
+
+
+
+
+
+
+
+
